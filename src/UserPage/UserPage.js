5c72ff3d31bac407b7ffe0d00a5543befd7206b8
Amy Amrani <vesamrani@gmail.com>
2020-12-12 15:52:55 -0500

    updated the user page to fetch the user and products from the backend

--- src/UserPage/UserPage.js
+++ src/UserPage/UserPage.js
@@ -1,27 +1,96 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import APIContext from '../APIContext';
 import Product from '../Product/Product';
+import config from '../config';
 
 class UserPage extends Component {
+  static contextType = APIContext;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+      user: undefined,
+      error: undefined,
+    }
+  };
+
+  componentDidMount () {
+    this.getUser();
+  }
+
+  getUser = () => {
+    const authToken = localStorage.getItem('authToken');
+
+    fetch(`${config.API_BASE_URL}/api/users/${this.props.userId}`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${authToken}`,
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
+        return res.json()
+      })
+      .then((res) => {
+        this.setState({ user: res })
+        this.getUserProducts();
+      })
+      .catch(error => this.setState({ error }))
+  };
+
+  getUserProducts = () => {
+    const authToken = localStorage.getItem('authToken')
+
+    fetch(`${config.API_BASE_URL}/api/products?user_id=${this.state.user.id}`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${authToken}`,
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
+        return res.json()
+      })
+      .then((res) => {
+        this.setState({ products: res })
+      })
+      .catch(error => this.setState({ error }))
+  };
+
   render() {
-    const { user, products } = this.props;
+    if (this.context.isLoggedIn === false) {
+      return <Redirect to='/' />
+    }
+
+    const { user, products } = this.state;
 
     return (
       <div className='page-container'>
+        {user && (
+          <section>
+            <h1 className='section-title'>{user.first_name} {user.last_name}'s <br /> Recommendations</h1>
+          </section>
+        )}
 
-        <section>
-          <h1 className='section-title'>{user.name}</h1>
-        </section>
-
-        <section>
-          <h2>Product Recommendations</h2>
-
-          {products.map(product => (
-            <div key={product.id}>
-              <Product product={product} />
-              <hr />
-            </div>
-          ))}
-        </section>
+        {products && (
+          <section>
+            {products.map(product => (
+              <div key={product.id}>
+                <Product product={product} />
+                <hr />
+              </div>
+            ))}
+          </section>
+        )}
       </div>
     );
   }
