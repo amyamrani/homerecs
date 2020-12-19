39e9e8228864f7a8a3871b67b94826f37da24c60
Amy Amrani <vesamrani@gmail.com>
2020-12-18 23:40:36 -0500

    updated logo icons on nav bar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -11,10 +11,10 @@ class Nav extends Component {
       <nav className={`nav ${this.props.location.pathname === '/' ? '' : 'nav-dark'}`}>
         <div>
           <Link to='/' className='logo-icon'>
-            <svg className='' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+            <svg className='logo-icon-home' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' />
             </svg>
-            <svg className='' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+            <svg className='logo-icon-sparkle' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z' />
             </svg>
           </Link>
